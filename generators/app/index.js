7acbfa0c5efab7664fd299bf79d39262f4fc8b77
Matheus Webler <matheus.webler@gmail.com>
2017-05-08 05:04:17 -0300

    working on swagger support

--- generators/app/index.js
+++ generators/app/index.js
@@ -22,7 +22,7 @@ module.exports = class extends Generator {
             this.fs.copyTpl(
                 this.templatePath('./includes/route.ejs'),
                 this.destinationPath(`./generatedcode/routes/${route.name}.js`),
-                { route: route });
+                { route: route, util: require('util') });
         }, this);
 
         if(this._initOptions.app.database){
