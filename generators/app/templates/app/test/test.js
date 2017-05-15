55fb61ceef9e160f8e84feec8933d050be5bbcd3
Matheus Webler <matheus.webler@gmail.com>
2017-05-15 17:39:21 -0300

    minor fixes

--- generators/app/templates/app/test/test.js
+++ generators/app/templates/app/test/test.js
@@ -7,6 +7,7 @@ const expect = chai.expect;
 describe('My avantjs test case', () => {
     beforeEach((done) => {
         //Do before tests
+        done();
     });
     describe('My test', () => {
         it('it expects something', (done) => {
