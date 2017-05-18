26fa80ad93e7225440a4d70b06f0a2131f09ecdf
Matheus Webler <matheus.webler@gmail.com>
2017-05-18 00:03:39 -0300

    updated for publish

--- generators/app/index.js
+++ generators/app/index.js
@@ -3,79 +3,78 @@ const Generator = require('yeoman-generator');
 const rename = require('gulp-rename');
 const beautify = require('gulp-beautify');
 const filter = require('gulp-filter');
-const beautifyOptions = { indent_size: 2 };
+const beautifyOptions = {indent_size: 2};
 
 module.exports = class extends Generator {
-    writing() {
+  writing() {
+    this.registerTransformStream(rename(path => {
+      if (path.extname === '.ejs') {
+        path.extname = '.js';
+      }
+    }));
 
-        this.registerTransformStream(rename(path => {
-            if (path.extname === '.ejs') {
-                path.extname = '.js';
-            }
-        }));
+    var ejsFilter = filter(['**/*.js', '**/*.json'], {restore: true});
 
-        var ejsFilter = filter(['**/*.js', '**/*.json'], { restore: true });
+    this.registerTransformStream([
+      ejsFilter,
+      beautify(beautifyOptions),
+      ejsFilter.restore
+    ]);
 
-        this.registerTransformStream([
-            ejsFilter,
-            beautify(beautifyOptions),
-            ejsFilter.restore
-        ]);
-
-        this.fs.copyTpl(
+    this.fs.copyTpl(
             this.templatePath('./app/**/*'),
             this.destinationPath('./generatedcode'),
             this._initOptions.app
         );
 
-        this._initOptions.app.server.routes.forEach(function (route) {
-            this.fs.copyTpl(
+    this._initOptions.app.server.routes.forEach(function (route) {
+      this.fs.copyTpl(
                 this.templatePath('./includes/route.ejs'),
                 this.destinationPath(`./generatedcode/routes/${route.name}.js`),
-                { route: route });
-        }, this);
+                {route: route});
+    }, this);
 
-        if (this._initOptions.app.database) {
-            this._initOptions.app.database.models.forEach(function (model) {
-                this.fs.copyTpl(
+    if (this._initOptions.app.database) {
+      this._initOptions.app.database.models.forEach(function (model) {
+        this.fs.copyTpl(
                     this.templatePath('./includes/model.ejs'),
                     this.destinationPath(`./generatedcode/models/${model.name}.js`),
-                    { model: model });
-            }, this);
-        }
+                    {model: model});
+      }, this);
+    }
 
-        if (this._initOptions.app.server.swagger) {
-            this.fs.copy(
+    if (this._initOptions.app.server.swagger) {
+      this.fs.copy(
                 this.templatePath('./public/**/*'),
                 this.destinationPath('./generatedcode/public')
             );
 
-            this.fs.copyTpl(
+      this.fs.copyTpl(
                 this.templatePath('./includes/swagger.ejs'),
                 this.destinationPath('./generatedcode/public/docs/swagger.json'),
-                { swagger: this._initOptions.app.server.swagger });
-        }
+                {swagger: this._initOptions.app.server.swagger});
+    }
 
-        this.fs.copy(
+    this.fs.copy(
             this.templatePath('./app/.eslint*'),
             this.destinationPath('./generatedcode')
         );
 
-        this.fs.copy(
+    this.fs.copy(
             this.templatePath('./app/.travis.yml'),
             this.destinationPath('./generatedcode/.travis.yml')
         );
 
-        this.fs.copy(
+    this.fs.copy(
             this.templatePath('./app/.gitignore'),
             this.destinationPath('./generatedcode/.gitignore')
         );
-    }
+  }
 
-    install() {
-        this.installDependencies({
-            npm: true,
-            bower: false
-        });
-    }
+  install() {
+    this.installDependencies({
+      npm: true,
+      bower: false
+    });
+  }
 };
