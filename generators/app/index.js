a69f6069771417bf69c4dc234881d690ec236e7f
Matheus Webler <matheus.webler@gmail.com>
2017-05-22 03:24:32 -0300

    fixed gitignore file generation

--- generators/app/index.js
+++ generators/app/index.js
@@ -66,7 +66,7 @@ module.exports = class extends Generator {
         );
 
     this.fs.copy(
-            this.templatePath('./app/ignore.git'),
+            this.templatePath('./app/.ignore.git'),
             this.destinationPath('./generatedcode/.gitignore')
         );
   }
